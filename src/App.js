8b4d4f2fff314f56d74fc336fb88f97891cd786d
Arijit123Muk <ben.arijit@hotmail.com>
2021-03-08 11:34:43 +0530

    Home Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './shared/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />  
-        
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
